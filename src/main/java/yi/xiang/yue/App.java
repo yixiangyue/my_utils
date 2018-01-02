d0d579095bf921c0005c3844ea442f84b8a8ee78
yixiangyue <yixiangyue@126.com>
2018-01-02 15:46:43 +0800

    '提交第一个项目'

---
+++ src/main/java/yi/xiang/yue/App.java
@@ -0,0 +1,13 @@
+package yi.xiang.yue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
